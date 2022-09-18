d18f3c1bfb2e15a9b694f2594644d23a289a12b6
Rafiul Hasan Tonmoy <92716668+rhtonmoy1@users.noreply.github.com>
2022-09-19 05:40:36 +0600

    Update index.php

--- Cart/index.php
+++ Cart/index.php
@@ -133,7 +133,7 @@
 
             </div>
         </section>
-        <?php  print_r($value); ?>
+
         <?php include_once('footer.php') ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
